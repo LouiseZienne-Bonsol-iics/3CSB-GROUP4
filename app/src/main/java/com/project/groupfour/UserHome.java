139efaf8796446c201cef7a98c17135ec4c45ed4
KentCarlBenjoGener <kentcarl.gener.iics@ust.edu.ph>
2020-11-06 22:09:11 +0800

    Added Slider to the Home Page

--- app/src/main/java/com/project/groupfour/UserHome.java
+++ app/src/main/java/com/project/groupfour/UserHome.java
@@ -6,6 +6,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Gravity;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserHome extends AppCompatActivity {
 
     @Override
@@ -17,5 +23,16 @@ public class UserHome extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
         //getSupportActionBar().setIcon(R.drawable.coffee);
+
+        ImageSlider imageSlider = findViewById(R.id.slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel("https://c.ndtvimg.com/2019-02/7ivu40vg_tea_625x300_08_February_19.jpg"));
+        slideModels.add(new SlideModel("cdn.slashgear.com/wp-content/uploads/2019/07/coffee_main_envat-1280x720.jpg"));
+        slideModels.add(new SlideModel("https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1062&q=80"));
+        slideModels.add(new SlideModel("https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/20/istock-157528129.jpg?width=990"));
+        slideModels.add(new SlideModel("https://upload.wikimedia.org/wikipedia/commons/5/54/Ipoh_White_Coffee%2C_Old_Town_Kopitiam_in_Australia.jpg"));
+        imageSlider.setImageList(slideModels, true);
+
     }
 }
+
