a3300083a684679e119dadcfd717f7c27f5a53f4
LouiseZienne-Bonsol-iics <louisezienne.bonsol.iics@ust.edu.ph>
2020-11-20 22:35:29 +0800

    Navigation Drawer with non-functional buttons

--- app/src/main/java/com/project/groupfour/UserHome.java
+++ app/src/main/java/com/project/groupfour/UserHome.java
@@ -1,7 +1,10 @@
 package com.project.groupfour;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Gravity;
@@ -13,17 +16,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserHome extends AppCompatActivity {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //toolbar.setTitle("Home Brew");
-
         setSupportActionBar(toolbar);
-        //getSupportActionBar().setIcon(R.drawable.coffee);
 
+        //Side Navigation Drawer
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //Featured Drinks Image Slider
         ImageSlider imageSlider = findViewById(R.id.slider);
         List<SlideModel> slideModels = new ArrayList<>();
         slideModels.add(new SlideModel("https://c.ndtvimg.com/2019-02/7ivu40vg_tea_625x300_08_February_19.jpg"));
@@ -32,7 +41,15 @@ public class UserHome extends AppCompatActivity {
         slideModels.add(new SlideModel("https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/04/09/20/istock-157528129.jpg?width=990"));
         slideModels.add(new SlideModel("https://upload.wikimedia.org/wikipedia/commons/5/54/Ipoh_White_Coffee%2C_Old_Town_Kopitiam_in_Australia.jpg"));
         imageSlider.setImageList(slideModels, true);
+    }
 
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
     }
 }
 
