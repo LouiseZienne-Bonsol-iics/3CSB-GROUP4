4e83a8ce8ad8f51458804f39baf4579676b57597
LouiseZienne-Bonsol-iics <louisezienne.bonsol.iics@ust.edu.ph>
2020-11-22 01:22:17 +0800

    Added current username to show in Navigation Drawer

--- app/src/main/java/com/project/groupfour/UserHome.java
+++ app/src/main/java/com/project/groupfour/UserHome.java
@@ -11,8 +11,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.project.groupfour.fragments.FavoritesFragment;
 import com.project.groupfour.fragments.HomeFragment;
 import com.project.groupfour.fragments.SearchFragment;
@@ -20,6 +28,11 @@ import com.project.groupfour.fragments.SettingsFragment;
 
 public class UserHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
+    private TextView currentUsername;
+
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +40,33 @@ public class UserHome extends AppCompatActivity implements NavigationView.OnNavi
         setContentView(R.layout.activity_user_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);    //hide status bar
+
+        //initialize firebase objects
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        //Users db directory in firebase
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.getUid());
 
         //Side Navigation Drawer
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        currentUsername = navigationView.getHeaderView(0).findViewById(R.id.nav_currentUser);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //get username from firebase db
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                currentUsername.setText(dataSnapshot.child("username").getValue().toString());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        //opening and closing of nav drawer
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
