e0d5c28a5ce6be381c4173c2703ab0f445649a8b
RicardoLuis-Bravo-iics <ricardoluis.bravo.iics@ust.edu.ph>
2020-11-22 19:21:59 +0800

    Non-functional Settings Page. -Done

---
+++ app/src/main/java/com/project/groupfour/UserSettings.java
@@ -0,0 +1,15 @@
+package com.project.groupfour;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_row);
+    }
+}
