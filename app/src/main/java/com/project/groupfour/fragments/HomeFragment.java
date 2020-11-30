938f3720453447f13341565db5ffbbd65a3fb559
KentCarlBenjoGener <kentcarl.gener.iics@ust.edu.ph>
2020-11-30 11:45:34 +0800

    Home now is scrollable and follows the format of the mock-up prototype Issue: Need to make the images collapsable / hideable as well as change it to gridview

--- app/src/main/java/com/project/groupfour/fragments/HomeFragment.java
+++ app/src/main/java/com/project/groupfour/fragments/HomeFragment.java
@@ -57,9 +57,9 @@ public class HomeFragment extends Fragment {
     private List <HomeConstructor> initData() {
 
         itemList = new ArrayList<>();
-        itemList.add(new HomeConstructor(R.drawable.ic_launcher_background,"Coffee"));
-        itemList.add(new HomeConstructor(R.drawable.ic_launcher_background,"Tea"));
-        itemList.add(new HomeConstructor(R.drawable.ic_launcher_background,"Juice"));
+        itemList.add(new HomeConstructor(R.drawable.ic_launcher_background,"Popular Now"));
+        itemList.add(new HomeConstructor(R.drawable.ic_launcher_background,"Recent Searches"));
+        itemList.add(new HomeConstructor(R.drawable.ic_launcher_background,"My Favorites"));
 
         return itemList;
 
