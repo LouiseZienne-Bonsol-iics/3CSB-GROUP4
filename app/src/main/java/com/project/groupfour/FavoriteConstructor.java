0f7519887288f12f91a547ed0eed3159ccbaf0fa
KentCarlBenjoGener <kentcarl.gener.iics@ust.edu.ph>
2020-12-04 21:21:08 +0800

    Manual Merge

---
+++ app/src/main/java/com/project/groupfour/FavoriteConstructor.java
@@ -0,0 +1,25 @@
+package com.project.groupfour;
+
+public class FavoriteConstructor {
+    int cFavoriteImage;
+    String cFavoriteName;
+
+    public FavoriteConstructor(int cFavoriteImage, String cFavoriteName) {
+        this.cFavoriteImage = cFavoriteImage;
+        this.cFavoriteName = cFavoriteName;
+    }
+
+    public int getcFavoriteImage() {
+        return cFavoriteImage;
+    }
+
+    public void setcFavoriteImage(int cFavoriteImage) { this.cFavoriteImage = cFavoriteImage; }
+
+    public String getcFavoriteName() {
+        return cFavoriteName;
+    }
+
+    public void setcFavoriteName(String cFavoriteName) {
+        this.cFavoriteName = cFavoriteName;
+    }
+}
