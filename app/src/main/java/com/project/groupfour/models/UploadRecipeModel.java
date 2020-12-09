ccff0793e0dffca5c7a563f5234111de5b069a82
LouiseZienne-Bonsol-iics <louisezienne.bonsol.iics@ust.edu.ph>
2020-12-09 11:47:09 +0800

    Add Recipe fully functional (except for "Delete" button)

--- app/src/main/java/com/project/groupfour/models/UploadRecipeModel.java
+++ app/src/main/java/com/project/groupfour/models/UploadRecipeModel.java
@@ -2,6 +2,8 @@ package com.project.groupfour.models;
 
 public class UploadRecipeModel {
     private String recipeName;
+    private String category;
+    private String subCategory;
     private String recipeRating;
     private String prepTime;
     private String ingredients;
@@ -12,12 +14,14 @@ public class UploadRecipeModel {
         //empty constructor needed
     }
 
-    public UploadRecipeModel(String recipeName, String recipeRating, String prepTime, String ingredients, String recipe, String imageUrl) {
+    public UploadRecipeModel(String recipeName, String category, String subCategory, String recipeRating, String prepTime, String ingredients, String recipe, String imageUrl) {
         if(recipeName.trim().equals("")){
             recipeName = "Unnamed Recipe";
         }
 
         this.recipeName = recipeName;
+        this.category = category;
+        this.subCategory = subCategory;
         this.recipeRating = recipeRating;
         this.prepTime = prepTime;
         this.ingredients = ingredients;
@@ -31,6 +35,22 @@ public class UploadRecipeModel {
 
     public void setRecipeName(String recipeName) {
         this.recipeName = recipeName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubCategory() {
+        return subCategory;
+    }
+
+    public void setSubCategory(String subCategory) {
+        this.subCategory = subCategory;
     }
 
     public String getRecipeRating() {
