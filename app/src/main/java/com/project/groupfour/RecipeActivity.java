88c70314ea4fd2e127a2657be2df32a958603ee0
LouiseZienne-Bonsol-iics <louisezienne.bonsol.iics@ust.edu.ph>
2020-12-12 13:12:34 +0800

    Favorites fragment fully functional

--- app/src/main/java/com/project/groupfour/RecipeActivity.java
+++ app/src/main/java/com/project/groupfour/RecipeActivity.java
@@ -22,6 +22,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,6 +31,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class RecipeActivity extends AppCompatActivity {
     private ImageView recipeImg;
@@ -52,6 +54,12 @@ public class RecipeActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     private TextView toolName;
+    String favItem;
+    boolean checkFav;
+
+    DatabaseReference RecipeRef;
+    DatabaseReference UserRef;
+    FirebaseAuth mAuth;
 
     DatabaseReference ref;
 
@@ -84,11 +92,13 @@ public class RecipeActivity extends AppCompatActivity {
             editRecipe.setVisibility(View.VISIBLE);
         }
 
-        ref = FirebaseDatabase.getInstance().getReference().child("Recipes");
+        RecipeRef = FirebaseDatabase.getInstance().getReference().child("Recipes");
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        mAuth = FirebaseAuth.getInstance();
 
         RecipeKey = getIntent().getStringExtra("RecipeKey");
 
-        ref.child(RecipeKey).addValueEventListener(new ValueEventListener() {
+        RecipeRef.child(RecipeKey).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if(dataSnapshot.exists()){
@@ -100,6 +110,7 @@ public class RecipeActivity extends AppCompatActivity {
                     recip = dataSnapshot.child("recipe").getValue().toString();
 
                     String tb = rName;
+                    favItem = rName;
 
                     Picasso.get().load(img).into(recipeImg);
                     recipeName.setText(rName);
@@ -120,11 +131,37 @@ public class RecipeActivity extends AppCompatActivity {
         });
     }
 
+    public void addToFavorites(View v) {
+        UserRef.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String UserKey = getIntent().getStringExtra("RecipeKey");
+                checkFav = dataSnapshot.child("Favorites").hasChild(UserKey);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        String UserKey = getIntent().getStringExtra("RecipeKey");
+        HashMap<String, Object> map = new HashMap<>();
+        //map.put(favItem, UserKey);
+        map.put("recipeID", UserKey);
+        map.put("recipeName", favItem);
+        map.put("imageUrl", img);
+        if(!checkFav){
+            Toast.makeText(this, "Added to favorites", Toast.LENGTH_SHORT).show();
+            UserRef.child(mAuth.getCurrentUser().getUid()).child("Favorites").child(UserKey).updateChildren(map);
+        }else{
+            Toast.makeText(this, "Removed from favorites", Toast.LENGTH_SHORT).show();
+            UserRef.child(mAuth.getCurrentUser().getUid()).child("Favorites").child(UserKey).removeValue();
+        }
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            /*Intent i = new Intent(RecipeActivity.this,UserHome.class);
-            startActivity(i);*/
             finish(); // close this activity and return to preview activity (if there is any)
         }
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/project/groupfour/fragments/FavoritesFragment.java
+++ app/src/main/java/com/project/groupfour/fragments/FavoritesFragment.java
@@ -1,51 +1,94 @@
 package com.project.groupfour.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.project.groupfour.adapters.FavoriteAdapter;
-import com.project.groupfour.FavoriteConstructor;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.project.groupfour.RecipeActivity;
 import com.project.groupfour.R;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.project.groupfour.models.RecipeModel;
+import com.squareup.picasso.Picasso;
 
 public class FavoritesFragment extends Fragment {
     public FavoritesFragment(){
 
     }
-    LinearLayout linearLayout;
+
     RecyclerView recyclerView;
-    List<FavoriteConstructor> itemList;
+
+    private DatabaseReference mFavRef;
+    private FirebaseAuth mAuth;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return inflater.inflate(R.layout.fragment_favorites, container, false);
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        mFavRef = FirebaseDatabase.getInstance().getReference().child("Users");//.child("Favorites");
+        mAuth = FirebaseAuth.getInstance();
+        mFavRef.keepSynced(true);
+
         recyclerView = view.findViewById(R.id.favRecyclerContent);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        recyclerView.setAdapter(new FavoriteAdapter(initData()));
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 4));
 
         return view;
     }
 
-    private List<FavoriteConstructor> initData() {
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        // Need to change first param to list or something
-        itemList = new ArrayList<>();
-        itemList.add(new FavoriteConstructor(R.drawable.ic_launcher_background,"My Favorites"));
+        FirebaseRecyclerAdapter<RecipeModel, ViewHolderClass> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<RecipeModel, ViewHolderClass>
+                (RecipeModel.class, R.layout.favorite_row, ViewHolderClass.class, mFavRef.child(mAuth.getCurrentUser().getUid()).child("Favorites")) {
+            @Override
+            protected void populateViewHolder(ViewHolderClass viewHolderClass, RecipeModel recipeModel, final int i) {
+                viewHolderClass.setRecipeName(recipeModel.getRecipeName());
+                viewHolderClass.setImage(recipeModel.getImageUrl());
 
-        return itemList;
+                viewHolderClass.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getActivity(), RecipeActivity.class);
+                        intent.putExtra("RecipeKey", getRef(i).getKey());
+                        startActivity(intent);
+                    }
+                });
+            }
+        };
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
 
+    public static  class ViewHolderClass extends RecyclerView.ViewHolder{
+        View mView;
+
+        public ViewHolderClass(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setRecipeName(String name){
+            TextView recName = (TextView) itemView.findViewById(R.id.fav_txt);
+            recName.setText(name);
+        }
+        public void setImage(String image){
+            ImageView recImage = (ImageView)itemView.findViewById(R.id.fav_img);
+            Picasso.get().load(image).into(recImage);
+        }
     }
 }
